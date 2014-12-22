61da93019c2854e31649492a18c5d881384d04af
aronovgj <aronovgj@gmx.net>
2014-12-22 20:01:38 +0100

    update crystaldiskinfo
    update crystaldiskinfo

--- crystaldiskinfo/tools/chocolateyInstall.ps1
+++ crystaldiskinfo/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+$packageName = 'crystaldiskinfo'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.sourceforge.jp/frs/redir.php?m=iij&f=%2Fcrystaldiskinfo%2F61888%2FCrystalDiskInfo6_2_2-en.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
