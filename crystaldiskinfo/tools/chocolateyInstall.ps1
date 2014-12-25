8f5c2a28b3bb190e21288e40ccf6df559cb488b8
aronovgj <aronovgj@gmx.net>
2014-12-25 01:54:23 +0100

    update
    update

--- crystaldiskinfo/tools/chocolateyInstall.ps1
+++ crystaldiskinfo/tools/chocolateyInstall.ps1
@@ -3,5 +3,4 @@ $installerType = 'EXE'
 $url = 'http://www.sourceforge.jp/frs/redir.php?m=iij&f=%2Fcrystaldiskinfo%2F61888%2FCrystalDiskInfo6_2_2-en.exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
