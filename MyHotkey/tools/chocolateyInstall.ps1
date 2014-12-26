9c3de393e36eb895e6ebffaf763a3db3369ec641
aronovgj <aronovgj@gmx.net>
2014-12-26 19:10:36 +0100

    updates, corrections,...
    updates, corrections,...

--- MyHotkey/tools/chocolateyInstall.ps1
+++ MyHotkey/tools/chocolateyInstall.ps1
@@ -1,10 +1,5 @@
 $packageName = 'myhotkey'
-#mkdir "$env:programfiles (x86)\MyHotkey"
-
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$binRoot = Get-BinRoot
-$destdir = Join-Path $binRoot $packageName
-
-#$destdir = "${env:programfiles(x86)}\MyHotkey"
 $url = 'https://onedrive.live.com/download?resid=9E3CAC6AAB07ABA0!10052&authkey=!ADmDXkkLoFnm-Mo&ithint=file%2czip'
+$destdir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 Install-ChocolateyZipPackage "$packageName" "$url" "$destdir"
+
