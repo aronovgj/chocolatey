59b2fd3d18bcee38f2a67e8a369a25c8e885ad5b
aronovgj <aronovgj@gmx.net>
2014-12-29 05:08:40 +0100

    add as ssd
    add as ssd

--- winhotkey/tools/chocolateyUninstall.ps1
+++ winhotkey/tools/chocolateyUninstall.ps1
@@ -1,12 +1,20 @@
 $packageName = 'winhotkey'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
-$uninstallerPath = "${env:programfiles(x86)}\WinHotKey\unins000.exe"
+if (Test-Path "${env:programfiles(x86)}\WinHotKey")
+{
+    $uninstallerPath = "${env:programfiles(x86)}\WinHotKey\unins000.exe"
+}
+else
+{
+	$uninstallerPath = "${env:programfiles}\WinHotKey\unins000.exe"
+}
+
 try {
-Stop-Process -ProcessName winhotkey*
-Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallerPath
-Write-ChocolateySuccess $packageName
-} catch {
-Write-ChocolateyFailure $packageName $($_.Exception.Message)
-throw
+    Stop-Process -ProcessName winhotkey*
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallerPath
+    Write-ChocolateySuccess $packageName
+    } catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
