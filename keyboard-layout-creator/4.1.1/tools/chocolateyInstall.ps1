fd5b0ed07e7079109e6a7cc8fab00decd6378625
aronovgj <aronovgj@gmx.net>
2015-12-03 01:18:02 +0100

    Revert "make keyboard-layout-creator fail on win server 2012"
    This reverts commit 99cc12cedbdb4070a9c85d1a3992fb1621aea91d.

--- keyboard-layout-creator/4.1.1/tools/chocolateyInstall.ps1
+++ keyboard-layout-creator/4.1.1/tools/chocolateyInstall.ps1
@@ -1,62 +1,17 @@
-function Test-OSVersion
-{
-    [CmdLetBinding()]
-    [OutputType([bool])]
-    param
-    (
-        [Parameter(Mandatory=$True)]
-        [version]$OSRequiredVersion,
-        [Parameter(Mandatory=$false)]
-        $CheckAgaintWorkstationVersionOnly=$True
-    )
+$packageName = 'keyboard-layout-creator'
+$fileType = 'msi'
+$silentArgs = '/qn'
+$url = 'http://download.microsoft.com/download/1/1/8/118aedd2-152c-453f-bac9-5dd8fb310870/MSKLC.exe'
 
-    [version]$currentOSVersion = [System.Environment]::OSVersion.Version
-    Write-Debug "Current OS Version: $currentOSVersion"
-    
-    [string]$operatingSystemCaption = (Get-WmiObject -class Win32_OperatingSystem).Caption
-    Write-Debug "Current OS Name: $operatingSystemCaption"
+$unpackDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$unpackFile = Join-Path $unpackDir 'mklc.zip'
 
-    [bool]$matchRequiredOsVersion = $false
+Get-ChocolateyWebFile $packageName $unpackFile $url
+Get-ChocolateyUnzip $unpackFile $unpackDir
+$fileMsi = Join-Path $unpackDir "MSKLC.msi"
+$fileExe = Join-Path $unpackDir "setup.exe"
 
-    if ($($currentOSVersion.Major -eq $OSRequiredVersion.Major) -and $($currentOSVersion.Minor -eq $OSRequiredVersion.Minor))
-    {
-        $matchRequiredOsVersion = $True
-
-        if (-not $CheckAgaintWorkstationVersionOnly)
-        {
-            [bool]$isWindowsServer = $operatingSystemCaption.ToUpperInvariant().Contains("SERVER")
-            $matchRequiredOsVersion = !$isWindowsServer
-        }
-    }
-
-    return $matchRequiredOsVersion
-}
-
-[bool]$isWindowsServer2012R2 = $(Test-OSVersion "6.3" $false)
-$isWindowsServer2012R2=$true
-if ($isWindowsServer2012R2)
-{
-    # [TODO] Improve the error message
-    throw "OS not supported."
-}
-else
-{
-    # Copy and paste from current package
-    $packageName = 'keyboard-layout-creator'
-    $fileType = 'msi'
-    $silentArgs = '/qn'
-    $url = 'http://download.microsoft.com/download/1/1/8/118aedd2-152c-453f-bac9-5dd8fb310870/MSKLC.exe'
-
-    $unpackDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-    $unpackFile = Join-Path $unpackDir 'mklc.zip'
-
-    Get-ChocolateyWebFile $packageName $unpackFile $url
-    Get-ChocolateyUnzip $unpackFile $unpackDir
-    $fileMsi = Join-Path $unpackDir "MSKLC.msi"
-    $fileExe = Join-Path $unpackDir "setup.exe"
-
-    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $fileMsi
-    Remove-Item $unpackFile -Recurse -Force
-    Remove-Item $fileExe -Recurse -Force
-    Remove-Item $fileMsi -Recurse -Force
-}
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $fileMsi
+Remove-Item $unpackFile -Recurse -Force
+Remove-Item $fileExe -Recurse -Force
+Remove-Item $fileMsi -Recurse -Force
