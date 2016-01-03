2464ccefd5b28d15368f2f88ff6840d809b8dc63
aronovgj <aronovgj@gmx.net>
2016-01-03 19:03:16 +0100

    add google books downloader. manual for now
    add google books downloader. manual for now

---
+++ google-books-downloader/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'google-books-downloader'
+$url        = 'https://googlebookdownloader.codeplex.com/downloads/get/1427613' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  url           = $url
+}
+
+Install-ChocolateyPackage @packageArgs
